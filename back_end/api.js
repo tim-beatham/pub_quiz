7348a1671f808ed1b89e04c3db295776c8cc7ea0
tim <beatham.tim@gmail.com>
2020-12-21 20:55:08 +0000

    started on the backend

---
+++ back_end/api.js
@@ -0,0 +1,77 @@
+const express = require("express");
+const app = express();
+
+const bodyParser = require("body-parser");
+
+const {postQuiz, fetchQuizzes} = require("./db_queries");
+
+app.use(bodyParser.json());
+
+const responseExpected = {
+    "quiz_name": "An example of a quiz",
+    "questions": []
+}
+
+const expectedQuestion = {
+    "question": "some question",
+    "answer": "some answer"
+}
+
+
+app.post("/api/quiz", function (req, res) {
+    const postRequest = req.body;
+
+    // Check the attributes of the request.
+    if (checkObjects(responseExpected, postRequest)) {
+        const questions = postRequest.questions;
+
+        for (let question of questions) {
+            if (!checkObjects(expectedQuestion, question)) {
+                res.status(400).send("invalid post request");
+                return;
+            }
+        }
+
+        postQuiz(postRequest);
+        res.status(200).send("successfully added");
+        return;
+    }
+    res.status(400).send("invalid post request");
+});
+
+
+app.get("/api/quizzes", function (req, res) {
+    fetchQuizzes()
+        .then(result => {
+            res.status(200).json(result);
+        }).catch(err => {
+            res.status(500).send(JSON.stringify(err));
+        });
+});
+
+
+console.log("Server Started");
+app.listen(9000);
+
+function checkObjects(obj1, obj2) {
+    const attributeNames1 = Object.getOwnPropertyNames(obj1);
+    const attributeNames2 = Object.getOwnPropertyNames(obj2);
+
+    if (attributeNames1.length !== attributeNames2.length)
+        return false;
+
+    for (let i = 0; i < attributeNames1.length; i++) {
+        let attributeName1 = attributeNames1[i];
+        let attributeName2 = attributeNames2[i];
+
+        if (attributeName1 !== attributeName2 || typeof obj1[attributeName1] !== typeof obj2[attributeName2]) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+
+
+
---
+++ back_end/db_queries.js
@@ -0,0 +1,39 @@
+const mongo = require("mongodb");
+
+const MongoClient = mongo.MongoClient;
+const url = "mongodb://localhost:27017/";
+
+
+
+
+
+function postQuiz(quiz) {
+    MongoClient.connect(url, function (err, db) {
+        if (err) throw err;
+
+        const dbObj = db.db("PubQuiz");
+
+        dbObj.collection("quizzes").insertOne(quiz, function (err, res) {
+            if (err) throw err;
+            console.log(`${quiz} inserted`);
+            db.close();
+        })
+    });
+}
+
+async function fetchQuizzes() {
+    let client, db;
+
+    client = await MongoClient.connect(url);
+    db = client.db("PubQuiz");
+
+    let result = await db.collection("quizzes").find({}, {projection: {"_id": 0}});
+    result = await result.toArray();
+
+    client.close();
+
+    return result;
+}
+
+exports.postQuiz = postQuiz;
+exports.fetchQuizzes = fetchQuizzes;
